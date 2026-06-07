f6e2c04cde0405baa4a070ad7c6dcc8b33efd4e7
KBaiju37 <karvirbaijunath@gmail.com>
2026-06-07 18:46:44 +0530

    first commit

---
+++ hello.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class hero{
+
+    private:
+    int num;
+    string name;
+    int hii;
+
+    public:
+    hero(){
+
+        cout<< "hello there buddy";
+    }
+
+    void display(int a){
+      cout<< "this is display function";
+     
+    }
+
+
+
+}
+
+int(){
+hero h;
+h.display();
+
+
+
+
+    return 0;
+}
