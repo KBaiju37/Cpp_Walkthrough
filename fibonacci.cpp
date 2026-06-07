072c1f32bb7aaf70e74f4395e818d3d0d8f61a01
KBaiju37 <karvirbaijunath@gmail.com>
2026-06-07 18:58:47 +0530

    how to implement fibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int fibonacci(int num) {
+    if (num == 0) {
+        return 0;
+    }
+    if (num == 1) {
+        return 1;
+    }
+    return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+int main() {
+    int a = fibonacci(7);
+    cout << a;
+    
+    return 0;
+}
